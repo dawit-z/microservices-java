3a1db4368441289eeec31b7a0a980733f9f2f536
Dawit <dzewelday@me.com>
2022-09-21 21:44:59 -0400

    Finished impl of security & API Gateway

--- order-service/src/main/java/com/dawit/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/dawit/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.dawit.orderservice.controller;
 
 import com.dawit.orderservice.dto.OrderRequest;
 import com.dawit.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +16,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Something went wrong.. Please try again in 10 minutes.";
     }
 }
